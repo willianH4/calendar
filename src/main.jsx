8fed95903a05a67856e7a1155975cd6555b7157f
Willian <williamhernandez450@gmail.com>
2023-06-14 07:20:22 -0600

    Add bootstrap and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { CalendarApp } from './CalendarApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CalendarApp />
   </React.StrictMode>,
 )
